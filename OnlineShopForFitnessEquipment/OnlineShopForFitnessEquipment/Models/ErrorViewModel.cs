39758b463ecd0e0d01fda8db9893723dec1ad9ce
FilipNedelchev20 <116354549+FilipNedelchev20@users.noreply.github.com>
2024-12-10 11:20:43 +0200

    Delete OnlineShopForFitnessEquipment directory

--- OnlineShopForFitnessEquipment/OnlineShopForFitnessEquipment/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OnlineShopForFitnessEquipment.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
