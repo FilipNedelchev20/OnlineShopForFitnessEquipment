a522d4cbfe7ae65c986984b74b7a01085776b9bc
FIlipNedelchev20 <filnedelchev@gmail.com>
2025-02-11 09:59:44 +0200

    Seeding the admin role

---
+++ FitnessEquipmentShop/FitnessEquipmentShop.Data.Seed/IdentityInitializer.cs
@@ -0,0 +1,45 @@
+﻿using FitnessEquipmentShop.Data.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace FitnessEquipmentShop.Data.Seed
+{
+    public static class IdentityInitializer
+    {
+        public static async Task SeedRolesAndAdminAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+
+            string[] roleNames = { "Admin", "User" };
+
+            foreach (var roleName in roleNames)
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            var adminUser = await userManager.FindByEmailAsync("admin@example.com");
+            if (adminUser == null)
+            {
+                var user = new User
+                {
+                    UserName = "admin",
+                    Email = "admin@example.com",
+                    FullName = "Admin User",
+                    EmailConfirmed = true
+                };
+
+                var result = await userManager.CreateAsync(user, "Admin123!"); 
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "Admin");
+                }
+            }
+        }
+    }
+}
