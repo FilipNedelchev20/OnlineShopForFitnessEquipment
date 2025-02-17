805dc1b7bcbee9753cfda413503585e53f9f102c
FIlipNedelchev20 <filnedelchev@gmail.com>
2025-02-17 10:54:02 +0200

    Fixing the User model

--- FitnessEquipmentShop/FitnessEquipmentShop.Data.Models/Entities/User.cs
+++ FitnessEquipmentShop/FitnessEquipmentShop.Data.Models/Entities/User.cs
@@ -5,6 +5,6 @@ namespace FitnessEquipmentShop.Data.Models.Entities
 {
     public class User : IdentityUser 
     {
-        public string FullName { get; set; } 
+        public string FullName { get; set; }
     }
 }
