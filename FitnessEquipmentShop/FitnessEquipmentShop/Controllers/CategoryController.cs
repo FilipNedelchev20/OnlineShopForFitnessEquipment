f8f0f9b362b12e0873be5e0f0cc4a2b0dd1a0789
FIlipNedelchev20 <filnedelchev@gmail.com>
2025-03-10 12:15:14 +0200

    Making most methods async

--- FitnessEquipmentShop/FitnessEquipmentShop/Controllers/CategoryController.cs
+++ FitnessEquipmentShop/FitnessEquipmentShop/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using FitnessEquipmentShop.Data;
+using FitnessEquipmentShop.Data.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 public class CategoryController : Controller
 {
@@ -12,7 +15,26 @@ public class CategoryController : Controller
 
     public IActionResult Index()
     {
-        var categories = _context.Categories.ToList();
+        var categories =  _context.Categories.ToList();
         return View(categories);
+    }
+
+    [Authorize(Roles = "Admin")]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Create(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        return View(category);
     }
 }
