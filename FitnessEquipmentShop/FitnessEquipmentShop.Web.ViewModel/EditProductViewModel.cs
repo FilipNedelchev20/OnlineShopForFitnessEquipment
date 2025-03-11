578eed3a1987682c6d3792b2d8fe64aa3f4abb0a
FIlipNedelchev20 <filnedelchev@gmail.com>
2025-03-11 17:20:03 +0200

    Adding a EditProductViewModel

---
+++ FitnessEquipmentShop/FitnessEquipmentShop.Web.ViewModel/EditProductViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessEquipmentShop.Web.ViewModel
+{
+    public class EditProductViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(1000)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, 1000000)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int StockQuantity { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [StringLength(500)]
+        public string ImageUrl { get; set; }
+    }
+}
