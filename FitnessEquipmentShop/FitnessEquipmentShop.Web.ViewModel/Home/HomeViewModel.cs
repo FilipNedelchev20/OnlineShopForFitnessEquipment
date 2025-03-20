3f62e50674593c69f951c55add1691d8ac4f6fcc
FIlipNedelchev20 <filnedelchev@gmail.com>
2025-03-20 12:34:01 +0200

    Creating a Home view model

---
+++ FitnessEquipmentShop/FitnessEquipmentShop.Web.ViewModel/Home/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using FitnessEquipmentShop.Data.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessEquipmentShop.Web.ViewModel.Home
+{
+    public class HomeViewModel
+    {
+        public List<ProductViewModel> TopProducts { get; set; } = new List<ProductViewModel>();
+    }
+}
