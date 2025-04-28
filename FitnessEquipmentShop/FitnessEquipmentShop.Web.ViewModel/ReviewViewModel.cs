727915947d69d44e30f1f7cc8a6c319ff992bc2c
FIlipNedelchev20 <filnedelchev@gmail.com>
2025-04-28 17:34:06 +0300

    Creating the review view model

---
+++ FitnessEquipmentShop/FitnessEquipmentShop.Web.ViewModel/ReviewViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FitnessEquipmentShop.Web.ViewModel
+{
+    public class ReviewViewModel
+    {
+        public ReviewViewModel() { }
+
+        public int Id { get; set; }
+        public string? Comment {  get; set; }
+        public int Rating { get; set; }
+        public int ProductId { get; set; }
+    }
+}
