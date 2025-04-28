94f24fe94c1a1c44245b2448f22b8144ccbb5da5
FIlipNedelchev20 <filnedelchev@gmail.com>
2025-04-28 17:35:49 +0300

    Working on the review service

--- FitnessEquipmentShop/FitnessEquipmentShop.Services/Infrastructure/IReviewService.cs
+++ FitnessEquipmentShop/FitnessEquipmentShop.Services/Infrastructure/IReviewService.cs
@@ -4,9 +4,14 @@ using System.Threading.Tasks;
 
 namespace FitnessEquipmentShop.Services
 {
+
     public interface IReviewService
     {
         Task<IEnumerable<Review>> GetReviewsByProductIdAsync(int productId);
+        Task<Review> GetReviewByIdAsync(int reviewId);
         Task AddReviewAsync(Review review);
+        Task UpdateReviewAsync(Review review);
+        Task DeleteReviewAsync(int id);
     }
+
 }
