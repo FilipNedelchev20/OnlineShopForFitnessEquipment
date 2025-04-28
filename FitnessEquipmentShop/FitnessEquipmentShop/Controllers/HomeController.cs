1f8d07ed32b798423eb750f05c2a232df96cabee
FIlipNedelchev20 <filnedelchev@gmail.com>
2025-04-28 17:30:34 +0300

    Fixing the home controller

--- FitnessEquipmentShop/FitnessEquipmentShop/Controllers/HomeController.cs
+++ FitnessEquipmentShop/FitnessEquipmentShop/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 ﻿using FitnessEquipmentShop.Services;
 using FitnessEquipmentShop.Web.ViewModel.Home;
+using FitnessEquipmentShop.Web.ViewModel.Category;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,31 +9,47 @@ namespace FitnessEquipmentShop.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
 
-        public HomeController(ILogger<HomeController> logger, IProductService productService)
+        public HomeController(IProductService productService, ICategoryService categoryService)
         {
-            _logger = logger;
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         public async Task<IActionResult> Index()
         {
             var products = await _productService.GetAllProductsAsync();
-            var topProducts = products.OrderByDescending(p => p.Rating)
-                                       .Take(5)
-                                       .Select(p => new ProductViewModel
-                                       {
-                                           Id = p.Id,
-                                           Name = p.Name,
-                                           Price = p.Price,
-                                           ImageUrl = p.ImageUrl
-                                       })
-                                       .ToList();
+            var categories = await _categoryService.GetAllCategoriesAsync();
 
-            var viewModel = new HomeViewModel { TopProducts = topProducts };
-            return View(viewModel);
+            var topProducts = products
+                .OrderByDescending(p => p.Rating)
+                .Take(6)
+                .Select(p => new ProductViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl
+                })
+                .ToList();
+
+            var topCategories = categories
+                .Select(c => new CategoryViewModel
+                {
+                    Id = c.Id,
+                    Name = c.Name
+                })
+                .ToList();
+
+            var model = new HomeViewModel
+            {
+                TopProducts = topProducts,
+                TopCategories = topCategories
+            };
+
+            return View(model);
         }
     }
 }
