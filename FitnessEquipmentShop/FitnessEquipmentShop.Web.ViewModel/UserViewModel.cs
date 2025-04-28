adc0af2ef9e43072709383899e733a7b00e19b52
FIlipNedelchev20 <filnedelchev@gmail.com>
2025-04-29 00:29:06 +0300

    Working on the user model

---
+++ FitnessEquipmentShop/FitnessEquipmentShop.Web.ViewModel/UserViewModel.cs
@@ -0,0 +1,8 @@
+﻿public class UserViewModel
+{
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public List<string> Roles { get; set; }
+    public DateTimeOffset? LockoutEnd { get; set; }
+    public DateTime RegistrationDate { get; set; } // Трябва да го имаш в User entity-то!
+}
