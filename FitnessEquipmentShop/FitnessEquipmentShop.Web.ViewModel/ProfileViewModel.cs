31bcd6e4786d90f5bec965691459e07d6392f630
FIlipNedelchev20 <filnedelchev@gmail.com>
2025-04-28 17:33:40 +0300

    Creating a Profile view model

---
+++ FitnessEquipmentShop/FitnessEquipmentShop.Web.ViewModel/ProfileViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessEquipmentShop.Web.ViewModel
+{
+    public class ProfileViewModel
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public IFormFile ProfilePicture { get; set; }
+        public string CurrentPicturePath { get; set; }
+
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+
+}
