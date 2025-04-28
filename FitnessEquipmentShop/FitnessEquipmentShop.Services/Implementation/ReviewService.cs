6da5396cfa01a02da899f74e8a139f45aa60773b
FIlipNedelchev20 <filnedelchev@gmail.com>
2025-04-28 17:37:16 +0300

    Working on the review service

--- FitnessEquipmentShop/FitnessEquipmentShop.Services/Implementation/ReviewService.cs
+++ FitnessEquipmentShop/FitnessEquipmentShop.Services/Implementation/ReviewService.cs
@@ -17,7 +17,17 @@ namespace FitnessEquipmentShop.Services
 
         public async Task<IEnumerable<Review>> GetReviewsByProductIdAsync(int productId)
         {
-            return await _context.Reviews.Where(r => r.ProductId == productId).ToListAsync();
+            return await _context.Reviews
+                .Where(r => r.ProductId == productId)
+                .Include(r => r.User)
+                .ToListAsync();
+        }
+
+        public async Task<Review> GetReviewByIdAsync(int reviewId)
+        {
+            return await _context.Reviews
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(r => r.Id == reviewId);
         }
 
         public async Task AddReviewAsync(Review review)
@@ -25,5 +35,22 @@ namespace FitnessEquipmentShop.Services
             await _context.Reviews.AddAsync(review);
             await _context.SaveChangesAsync();
         }
+
+        public async Task UpdateReviewAsync(Review review)
+        {
+            _context.Reviews.Update(review);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteReviewAsync(int id)
+        {
+            var review = await _context.Reviews.FindAsync(id);
+            if (review != null)
+            {
+                _context.Reviews.Remove(review);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
+
 }
