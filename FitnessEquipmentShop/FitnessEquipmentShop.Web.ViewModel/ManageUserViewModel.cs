695e087df6eff7634b945da63e96545ec7e2c8d5
FIlipNedelchev20 <filnedelchev@gmail.com>
2025-04-29 00:29:20 +0300

    Workinng on the manage user view model

---
+++ FitnessEquipmentShop/FitnessEquipmentShop.Web.ViewModel/ManageUserViewModel.cs
@@ -0,0 +1,7 @@
+﻿public class ManageUserViewModel
+{
+    public string Email { get; set; }
+    public List<string> Roles { get; set; }
+    public DateTime RegisteredOn { get; set; }
+    public bool IsLockedOut { get; set; }
+}
