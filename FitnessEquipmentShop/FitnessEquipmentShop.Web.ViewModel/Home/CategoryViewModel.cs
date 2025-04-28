85cb7268a1b4bf45dc312e81e7c5262c39b2d921
FIlipNedelchev20 <filnedelchev@gmail.com>
2025-04-28 17:35:34 +0300

    Creating the category view model

---
+++ FitnessEquipmentShop/FitnessEquipmentShop.Web.ViewModel/Home/CategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FitnessEquipmentShop.Web.ViewModel.Category
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
